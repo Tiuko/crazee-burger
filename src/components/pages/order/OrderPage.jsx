13163fb7c64b79e4b1a16074a5559ea6f16284f5
Tiuko <108450912+Tiuko@users.noreply.github.com>
2023-12-05 10:32:21 +0100

    :construction: F04 : ORDER > layout > Add container

--- src/components/pages/order/OrderPage.jsx
+++ src/components/pages/order/OrderPage.jsx
@@ -1,17 +1,37 @@
-import {Link, useParams} from 'react-router-dom';
+import { Link, useParams } from "react-router-dom";
+import { theme } from "../../../theme/index.js";
+import styled from "styled-components";
 
 const OrderPage = () => {
+  // State
+  const { username } = useParams();
 
-	// State
-	const { username} = useParams()
-
-	// Render
-	return (
-		<div>
-			<h1>Bonjour {username}</h1>
-			<Link to="/"><button>Déconnexion</button></Link>
-		</div>
-	);
+  // Render
+  return (
+    <OrderPageStyled>
+      <div className="container">
+        <h1>Bonjour {username}</h1>
+        <Link to="/">
+          <button>Déconnexion</button>
+        </Link>
+      </div>
+    </OrderPageStyled>
+  );
 };
+
+const OrderPageStyled = styled.div`
+  background-color: ${theme.colors.primary};
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  
+  .container {
+	background-color: red;
+	height: 95vh;
+	width: 1400px;
+
+  }
+`;
 
 export default OrderPage;
