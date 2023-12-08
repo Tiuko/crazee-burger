5fe9441f8a04cc19b40650e1394ab9f6a51d0daa
Tiuko <108450912+Tiuko@users.noreply.github.com>
2023-12-08 11:45:34 +0100

    :construction: F05: ORDER > MENU > Add product info

--- src/components/pages/order/Main/Menu.jsx
+++ src/components/pages/order/Main/Menu.jsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 import { useState } from "react";
-import { fakeMenu1, fakeMenu2 } from "../../../../fakeData/fakeMenu.js";
-import {theme} from '../../../../theme/index.js';
+import { fakeMenu2 } from "../../../../fakeData/fakeMenu.js";
+import { theme } from "../../../../theme/index.js";
 
 const Menu = () => {
   const [menu, setMenu] = useState(fakeMenu2);
@@ -9,7 +9,20 @@ const Menu = () => {
   return (
     <MenuStyled>
       {menu.map((produit) => {
-        return <div className="produit">{produit.title}</div>;
+        return (
+          <div className="produit">
+            <div className="image">
+              <img src={produit.imageSource} alt={produit.title} />
+            </div>
+            <div className="info-text">
+              <div className="title">{produit.title}</div>
+              <div className="description">
+                <div className="price">{produit.price}</div>
+                <button className="add-button">Ajouter</button>
+              </div>
+            </div>
+          </div>
+        );
       })}
     </MenuStyled>
   );
@@ -23,11 +36,21 @@ const MenuStyled = styled.div`
   padding: 3.125rem 3.125rem 9.375rem;
   justify-items: center;
   box-shadow: 0 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
-  
+
   .produit {
     background: red;
     width: 15rem;
     height: 20.625rem;
+  }
+
+  .image {
+    border: 1px solid fuchsia;
+    width: 100px;
+    height: auto;
+    img {
+      width: 100%;
+      height: 100%;
+    }
   }
 `;
 
