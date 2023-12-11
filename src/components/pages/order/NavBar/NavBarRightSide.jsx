fb8d6d10ecf58c3d90e85d325087049e71afd773
Tiuko <108450912+Tiuko@users.noreply.github.com>
2023-12-11 18:30:13 +0100

    :sparkles: F06: NAVBAR > Add notification toast on admin button click

--- src/components/pages/order/NavBar/NavBarRightSide.jsx
+++ src/components/pages/order/NavBar/NavBarRightSide.jsx
@@ -1,14 +1,35 @@
-import styled from "styled-components";
 import PropTypes from "prop-types";
 import Profile from "./Profile.jsx";
 import ToggleButton from "../../../reusable-ui/ToggleButton.jsx";
+import { toast } from "react-toastify";
+import styled from "styled-components";
+import { useState } from "react";
 
 const NavBarRightSide = ({ username }) => {
+  const [isModeAdmin, setIsModeAdmin] = useState(false);
+
+  const displayToastNotification = () => {
+    if (!isModeAdmin) {
+      toast.info("Mode admin activé", {
+        theme: "dark",
+        position: "bottom-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    }
+    setIsModeAdmin(!isModeAdmin);
+  };
+
   return (
     <NavBarRightSideStyled className="right-side">
       <ToggleButton
         labelIfUnchecked="ACTIVER LE MODE ADMIN"
         labelIfChecked="DÉSACTIVER LE MODE ADMIN"
+        onToggle={displayToastNotification}
       />
       <Profile username={username} />
     </NavBarRightSideStyled>
@@ -20,10 +41,6 @@ const NavBarRightSideStyled = styled.div`
   align-items: center;
   padding: 50px;
   gap: 50px;
-
-  .profile {
-    background: yellow;
-  }
 `;
 
 NavBarRightSide.propTypes = {
