7084376d70907292b8fa571388bcd139c5b7791b
Tiuko <108450912+Tiuko@users.noreply.github.com>
2023-12-13 19:13:50 +0100

    :lipstick: F07: ADMIN > Add AdminTabs & AdminPanel

--- src/components/pages/order/Main/Admin/Admin.jsx
+++ src/components/pages/order/Main/Admin/Admin.jsx
@@ -1,19 +1,21 @@
 import styled from "styled-components";
-import { theme } from "../../../../../theme/index.js";
+import AdminPanel from "./AdminPanel.jsx";
+import AdminTabs from "./AdminTabs.jsx";
 
 const Admin = () => {
-  return <AdminStyled>Admin</AdminStyled>;
+  return (
+    <AdminStyled>
+      <AdminTabs />
+      <AdminPanel />
+    </AdminStyled>
+  );
 };
 
 const AdminStyled = styled.div`
-  height: 250px;
   position: absolute;
   bottom: 0;
   right: 0;
   left: 0;
-  background: ${theme.colors.white};
-  border: 1px solid ${theme.colors.greyLight};
-  box-shadow: ${theme.shadows.subtle};
 `;
 
 export default Admin;
---
+++ src/components/pages/order/Main/Admin/AdminTabs.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const AdminTabs = () => {
+  return <AdminTabsStyled>AdminTabs</AdminTabsStyled>;
+};
+
+const AdminTabsStyled = styled.div`
+  border: 3px solid pink;
+`;
+
+export default AdminTabs;
