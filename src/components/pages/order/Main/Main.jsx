8c6b93e980496bba69a6ec5db424dd837be04e97
Tiuko <108450912+Tiuko@users.noreply.github.com>
2023-12-13 16:02:27 +0100

    :lipstick: F07: MAIN > Add layout with Basket

--- src/components/pages/order/Main/Main.jsx
+++ src/components/pages/order/Main/Main.jsx
@@ -1,31 +1,50 @@
 import styled from "styled-components";
 import { theme } from "../../../../theme/index.js";
-import Menu from './Menu.jsx';
+import Menu from "./Menu.jsx";
 
 const Main = () => {
-  return <MainStyled className="main">
-    {/*<div className="basket">Basket</div>*/}
-    <Menu />
-  </MainStyled>;
-
+  return (
+    <MainStyled className="main">
+      {/*<div className="basket">Basket</div>*/}
+      <div className="menu-and-admin">
+        <Menu />
+        <div className="admin">Admin</div>
+      </div>
+    </MainStyled>
+  );
 };
 
 const MainStyled = styled.div`
+  border: 3px solid green;
   background: ${theme.colors.white};
+  height: calc(95vh - 10vh);
   box-shadow: 0 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
   flex: 1;
   border-bottom-right-radius: ${theme.borderRadius.extraRound};
   border-bottom-left-radius: ${theme.borderRadius.extraRound};
-  
   display: grid;
   grid-template-columns: 1fr;
+  //grid-template-columns: 25% 1fr;
 
-  overflow-y: scroll;
-  overflow-x: scroll;
-  //.basket {
-  //  background: pink;
-  //}
-  
+  .basket {
+    background: pink;
+  }
+
+  .menu-and-admin {
+    position: relative;
+    overflow-y: hidden;
+    display: grid;
+    border-bottom-right-radius: ${theme.borderRadius.extraRound};
+    border-bottom-left-radius: ${theme.borderRadius.extraRound};
+    .admin {
+      background: red;
+      height: 250px;
+      position: absolute;
+      bottom: 0;
+      right: 0;
+      left: 0;
+    }
+  }
 `;
 
 export default Main;
