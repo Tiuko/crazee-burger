f10c393af879ba328ddc2bbb4559eb62a3557532
Tiuko <108450912+Tiuko@users.noreply.github.com>
2023-12-19 16:43:20 +0100

    :recycle: F08: ADMIN > AddForm > Replace new Date with crypto.randomUUID()

--- src/components/pages/order/Main/Admin/AdminPanel/AddForm.jsx
+++ src/components/pages/order/Main/Admin/AdminPanel/AddForm.jsx
@@ -22,17 +22,15 @@ const AddForm = () => {
 
     const newProductToAdd = {
       ...newProduct,
-      id: new Date().getTime(),
+      id: crypto.randomUUID(),
     };
 
     handleAdd(newProductToAdd);
   };
 
   const handleChange = (event) => {
-    const newValue = event.target.value;
-    const name = event.target.name;
-
-    setNewProduct({ ...newProduct, [name]: newValue });
+    const { name, value } = event.target;
+    setNewProduct({ ...newProduct, [name]: value });
   };
 
   return (
