af95fbf319aedd22ad61e1496c94b8ba9019361c
Tiuko <108450912+Tiuko@users.noreply.github.com>
2024-01-05 12:30:52 +0100

    :recycle: F08: AddForm > Extract ImagePreview

--- src/components/pages/order/Main/Admin/AdminPanel/AddForm.jsx
+++ src/components/pages/order/Main/Admin/AdminPanel/AddForm.jsx
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-import { theme } from "../../../../../../theme/index.js";
 import OrderContext from "../../../../../../context/OrderContext.jsx";
 import { useContext, useState } from "react";
 import TextInput from "../../../../../reusable-ui/TextInput.jsx";
@@ -9,6 +8,7 @@ import { MdOutlineEuro } from "react-icons/md";
 import Button from "../../../../../reusable-ui/Button.jsx";
 import SubmitMessage from './SubmitMessage.jsx';
 import {EMPTY_PRODUCT} from '../../../../../../utils/helpers.js';
+import ImagePreview from './ImagePreview.jsx';
 
 const AddForm = () => {
   // State
@@ -43,13 +43,7 @@ const AddForm = () => {
 
   return (
     <AddFormStyled onSubmit={handleSubmit}>
-      <div className="image-preview">
-        {newProduct.imageSource ? (
-          <img src={newProduct.imageSource} alt={newProduct.title} />
-        ) : (
-          <div className="empty-image">Aucune image</div>
-        )}
-      </div>
+      <ImagePreview imageSource={newProduct.imageSource} title={newProduct.title}/>
       <div className="input-fields">
         <TextInput
           name="title"
@@ -99,33 +93,7 @@ const AddFormStyled = styled.form`
   width: 70%;
   grid-column-gap: 20px;
   grid-row-gap: 8px;
-
-  .image-preview {
-    grid-area: 1 / 1 / 4 / 2;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
-    img {
-      width: 100%;
-      height: 100%;
-      object-fit: contain;
-      object-position: center;
-    }
-
-    .empty-image {
-      height: 100%;
-      width: 100%;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      border: 1px solid ${theme.colors.greyLight};
-      line-height: 1.5;
-      color: ${theme.colors.greySemiDark};
-      border-radius: ${theme.borderRadius.round};
-    }
-  }
-
+  
   .input-fields {
     grid-area: 1 / 2 / 4 / 3;
 
---
+++ src/components/pages/order/Main/Admin/AdminPanel/ImagePreview.jsx
@@ -0,0 +1,48 @@
+import styled from "styled-components";
+import {theme} from '../../../../../../theme/index.js';
+import PropTypes from 'prop-types';
+
+const ImagePreview = ({imageSource, title}) => {
+ return (
+  <ImagePreviewStyled>
+          {imageSource ? (
+              <img src={imageSource} alt={title} />
+          ) : (
+              <div className="empty-image">Aucune image</div>
+          )}
+  </ImagePreviewStyled>
+ );
+};
+
+const ImagePreviewStyled = styled.div`
+        grid-area: 1 / 1 / 4 / 2;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        img {
+          width: 100%;
+          height: 100%;
+          object-fit: contain;
+          object-position: center;
+        }
+    
+        .empty-image {
+            height: 100%;
+            width: 100%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            border: 1px solid ${theme.colors.greyLight};
+            line-height: 1.5;
+            color: ${theme.colors.greySemiDark};
+            border-radius: ${theme.borderRadius.round};
+        }
+`;
+
+ImagePreview.propTypes = {
+    imageSource: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+}
+
+export default ImagePreview;
