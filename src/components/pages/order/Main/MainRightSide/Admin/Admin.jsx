d21c52354c4bbf3ecc40d5ece50196cf319d44d8
Tiuko <108450912+Tiuko@users.noreply.github.com>
2024-01-25 09:14:57 +0100

    :bug: F14 : ADMIN > Panel > add animation onAppear (with keyframes)

--- src/components/pages/order/Main/MainRightSide/Admin/Admin.jsx
+++ src/components/pages/order/Main/MainRightSide/Admin/Admin.jsx
@@ -3,6 +3,8 @@ import AdminPanel from "./AdminPanel/AdminPanel.jsx";
 import AdminTabs from "./AdminTabs.jsx";
 import { useContext } from "react";
 import OrderContext from "../../../../../../context/OrderContext.jsx";
+import { fadeInFromBottom } from "../../../../../../theme/animation.js";
+import { theme } from "../../../../../../theme/index.js";
 
 const Admin = () => {
   const { isCollapsed } = useContext(OrderContext);
@@ -20,6 +22,8 @@ const AdminStyled = styled.div`
   bottom: 0;
   right: 0;
   left: 0;
+
+  animation: ${fadeInFromBottom} ease-out ${theme.animations.speed.slow};
 `;
 
 export default Admin;
