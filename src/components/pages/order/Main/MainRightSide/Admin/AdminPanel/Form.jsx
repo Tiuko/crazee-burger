28107751e77a918836d6b26f96a295a99def7461
Tiuko <108450912+Tiuko@users.noreply.github.com>
2024-01-26 10:50:05 +0100

    :construction: F15 : FORM > Add select (ugly)

--- src/components/pages/order/Main/MainRightSide/Admin/AdminPanel/Form.jsx
+++ src/components/pages/order/Main/MainRightSide/Admin/AdminPanel/Form.jsx
@@ -23,6 +23,14 @@ const Form = React.forwardRef(
               ref={ref && input.name === "title" ? ref : null}
             />
           ))}
+          <select name="isAvailable" className="is-available" id="3">
+            <option value={true}>En stock</option>
+            <option value={false}>En rupture</option>
+          </select>
+          <select name="isPublicised" className="is-publicised" id="4">
+            <option value={true}>Sans pub</option>
+            <option value={false}>Avec pub</option>
+          </select>
         </div>
         <div className="form-footer">{children}</div>
       </FormStyled>
@@ -61,19 +69,25 @@ const FormStyled = styled.form`
     grid-template-rows: repeat(3, 1fr);
     grid-template-columns: repeat(3, 1fr);
     grid-row-gap: 8px;
-      
-      .title {
-          grid-area: 1 / 1 / 2 / 4;
-      }
- 
-      .image-source {
-          grid-area: 2 / 1 / 3 / 4;
-      }
-      
-      .price {
-          grid-area: 3 / 1 / 4 / 2;
-      }
-      
+    grid-column-gap: 8px;
+
+    .title {
+      grid-area: 1 / 1 / 2 / 4;
+    }
+
+    .image-source {
+      grid-area: 2 / 1 / 3 / 4;
+    }
+
+    .price {
+      grid-area: 3 / 1 / 4 / 2;
+    }
+
+    .is-available {
+    }
+
+    .is-publicised {
+    }
   }
 
   .form-footer {
