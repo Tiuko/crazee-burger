c24a8b67fdd2ecb813cdf98e7118a8ccfbd67508
Tiuko <108450912+Tiuko@users.noreply.github.com>
2024-01-26 17:12:58 +0100

    :construction: F15 : AVAILABILITY > BASKET > Card > Add "non disponible" if product is made unavailable

--- src/components/pages/order/Main/Basket/BasketBody/BasketProducts.jsx
+++ src/components/pages/order/Main/Basket/BasketBody/BasketProducts.jsx
@@ -1,13 +1,18 @@
 import { useContext } from "react";
 import styled from "styled-components";
-import { IMAGE_COMING_SOON } from "../../../../../../enums/product.js";
+import {
+  BASKET_MESSAGE,
+  IMAGE_COMING_SOON,
+} from "../../../../../../enums/product.js";
 import BasketCard from "./BasketCard.jsx";
 import OrderContext from "../../../../../../context/OrderContext.jsx";
 import { findObjectById } from "../../../../../../utils/array.js";
 import { checkIfProductIsClicked } from "../../MainRightSide/Menu/helper.js";
 import { formatPrice } from "../../../../../../utils/maths.js";
 import { TransitionGroup, CSSTransition } from "react-transition-group";
-import {basketAnimation} from '../../../../../../theme/animation.js';
+import { basketAnimation } from "../../../../../../theme/animation.js";
+import { convertStringToBoolean } from "../../../../../../utils/string.js";
+import Sticker from "../../../../../reusable-ui/Sticker.jsx";
 
 const BasketProducts = () => {
   const {
@@ -26,33 +31,54 @@ const BasketProducts = () => {
   };
 
   return (
-      <TransitionGroup component={BasketProductsStyled} className={"transition-group"}>
-        {basket.map((basketProduct) => {
-          const menuProduct = findObjectById(basketProduct.id, menu)
-          return (
-              <CSSTransition
-                  appear={true}
-                  classNames={"animation-basket"}
-                  key={basketProduct.id}
-                  timeout={300}
-              >
-                <div className="card-container">
-                  <BasketCard
-                      {...menuProduct}
-                      formattedPrice={formatPrice(menuProduct.price)}
-                      imageSource={menuProduct.imageSource ? menuProduct.imageSource : IMAGE_COMING_SOON}
-                      quantity={basketProduct.quantity}
-                      onDelete={(event) => handleOnDelete(event, basketProduct.id)}
-                      isClickable={isModeAdmin}
-                      onClick={isModeAdmin ? () => handleProductSelected(basketProduct.id) : null}
-                      isSelected={checkIfProductIsClicked(basketProduct.id, productSelected.id)}
-                      className={"card"}
-                  />
-                </div>
-              </CSSTransition>
-          )
-        })}
-      </TransitionGroup>
+    <TransitionGroup
+      component={BasketProductsStyled}
+      className={"transition-group"}
+    >
+      {basket.map((basketProduct) => {
+        const menuProduct = findObjectById(basketProduct.id, menu);
+        return (
+          <CSSTransition
+            appear={true}
+            classNames={"animation-basket"}
+            key={basketProduct.id}
+            timeout={300}
+          >
+            <div className="card-container">
+              {convertStringToBoolean(menuProduct.isPublicised) && (
+                <Sticker className="badge-new" />
+              )}
+              <BasketCard
+                {...menuProduct}
+                imageSource={
+                  menuProduct.imageSource
+                    ? menuProduct.imageSource
+                    : IMAGE_COMING_SOON
+                }
+                quantity={basketProduct.quantity}
+                onDelete={(event) => handleOnDelete(event, basketProduct.id)}
+                isClickable={isModeAdmin}
+                onClick={
+                  isModeAdmin
+                    ? () => handleProductSelected(basketProduct.id)
+                    : null
+                }
+                isSelected={checkIfProductIsClicked(
+                  basketProduct.id,
+                  productSelected.id,
+                )}
+                className={"card"}
+                price={
+                  convertStringToBoolean(menuProduct.isAvailable)
+                    ? formatPrice(menuProduct.price)
+                    : BASKET_MESSAGE.NOT_AVAILABLE
+                }
+              />
+            </div>
+          </CSSTransition>
+        );
+      })}
+    </TransitionGroup>
   );
 };
 
@@ -61,7 +87,7 @@ const BasketProductsStyled = styled.div`
   display: flex;
   flex-direction: column;
   overflow-y: scroll;
-  
+
   .card-container {
     margin: 10px 16px;
     height: 86px;
@@ -72,8 +98,16 @@ const BasketProductsStyled = styled.div`
     //:last-child {
     //  margin-bottom: 10px;
     //}
+
+    .badge-new {
+      position: absolute;
+      z-index: 1;
+      bottom: 10%;
+      left: 21%;
+      transform: translateY(-21%) translateX(-5%);
+    }
   }
-  
+
   ${basketAnimation}
 `;
 
